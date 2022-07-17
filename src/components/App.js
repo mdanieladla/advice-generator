5f9581a4440752954110b6aba382bdd6b3b15b58
mdanieladla <mariadanieladarnea@gmail.com>
2022-07-17 12:08:04 +0200

    imported Advice component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Advice from './Advice';
+//styles
+import '../styles/App.scss';
+const App = () => {
+  return (
+    <div className='test'>
+      <Advice />
+    </div>
+  );
+};
+
+export default App;
