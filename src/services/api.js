9bf43f9295c955bcaaf9ef358ec47ce7619e8e96
mdanieladla <mariadanieladarnea@gmail.com>
2022-08-01 17:44:07 +0200

    added chache:no store for making it work on firefox

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const callToApi = () => {
-  return fetch('https://api.adviceslip.com/advice')
+  return fetch('https://api.adviceslip.com/advice', { cache: 'no-store' })
     .then((response) => response.json())
     .then((response) => {
       const result = {
